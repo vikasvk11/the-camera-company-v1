94a600b9b82c89de32ad08c1192688c848a70469
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-05-17 10:09:22 +0530

    added NavBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
-import { NavLink } from "react-router-dom";
 import { useCart } from "./cart/CartContext";
 import { Router } from "./Routes";
+import { NavBar } from "./Components/NavBar";
 import axios from "axios";
 
 export default function App() {
@@ -66,44 +66,7 @@ export default function App() {
 
   return (
     <>
-      <nav className="nav-bar">
-        <img
-          src="images\cart-icon-v2.png"
-          className="nav-bar-logo"
-          alt="logo"
-        />
-        <h1>
-          <NavLink to="/products" className="nav-header">
-            the Camera Company
-          </NavLink>
-        </h1>
-        <ul className="nav-list">
-          <li>
-            <NavLink to="/cart">
-              <div className="badge-icon">
-                <i className="material-icons col">shopping_cart</i>
-                {cartState.cart.length === 0 ? (
-                  ""
-                ) : (
-                  <span>{cartState.cart.length}</span>
-                )}
-              </div>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/wishlist">
-              <div className="badge-icon">
-                <i className="material-icons col">favorite</i>
-                {cartState.wishlist.length === 0 ? (
-                  ""
-                ) : (
-                  <span>{cartState.wishlist.length}</span>
-                )}
-              </div>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
       <Router />
     </>
   );
